627a8d0d45932b63f185b38e551ffafdaa548315
warrenbrown <warren@katapultmedia.com>
2017-01-16 03:19:33 -0600

    Adding remote git

---
+++ app/models/appointment_slot.rb
@@ -0,0 +1,3 @@
+class AppointmentSlot < ActiveRecord::Base
+  belongs_to :appointment
+end
---
+++ test/models/appointment_slot_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AppointmentSlotTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
